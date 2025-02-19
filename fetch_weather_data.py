d7d403af2be82e942d561a5531f3d6fdbc4b73c7
nickStanisic <nicholasstanisic@gmail.com>
2025-02-19 11:29:20 -0700

    added cast to low and high so string comparison didn't through error

--- fetch_weather_data.py
+++ fetch_weather_data.py
@@ -8,16 +8,16 @@ def get_weather(min_lat, lat_increase, min_lon, lon_increase):
     weather_data = []
     for i in range(min_lat,min_lat - lat_increase, -1):
         for j in range(min_lon, min_lon + lon_increase, 1):
-            for n in range(0,4):
-                for l in range(0,4):
-                    lat = i + n/4
-                    lon = j + l/4
+            for n in range(0,8):
+                for l in range(0,8):
+                    lat = i + n/8
+                    lon = j + l/8
                     data = weatherCall(lat, lon)
                     populateWeatherData(data, weather_data, lat, lon)            
     #try to get data into postgresDB
     insertData(weather_data)
 
-schedule.every().day.at("17:50").do(get_weather(40,1,-109,1))
+schedule.every().day.at("17:50").do(get_weather(40,4,-109,7))
 
 print("Weather data retrieval started, waiting for scheduler")
 while True:
